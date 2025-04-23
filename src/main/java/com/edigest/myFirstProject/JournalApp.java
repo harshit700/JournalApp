8f59317a650990608c78890ca777d5684181d1d5
harshit700 <harshit32670@gmail.com>
2025-04-23 16:54:35 +0530

    Rename to JournalApp

--- src/main/java/com/edigest/myFirstProject/JournalApp.java
+++ src/main/java/com/edigest/myFirstProject/JournalApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyFirstProjectApplication {
+public class JournalApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyFirstProjectApplication.class, args);
+		SpringApplication.run(JournalApp.class, args);
 	}
 
 }
